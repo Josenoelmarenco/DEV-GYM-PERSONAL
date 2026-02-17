e7a32e7ef2f8229cd4212e5d54bec82464ff3652
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-02-17 15:38:22 +0200

    refactoring title component

--- week3/week3-fe-pair-programming/src/components/Title.jsx
+++ week3/week3-fe-pair-programming/src/components/Title.jsx
@@ -1,11 +1,21 @@
-function Title() {
-  return (
-    <div>
-      <p className="section-dummy">Text</p>
-      <p className="section-dummy">Text</p>
-    </div>
-  );
-}
+// function Title() {
+//   return (
+//     <div>
+//       <p className="section-dummy">Text</p>
+//       <p className="section-dummy">Text</p>
+//     </div>
+//   );
+// }
 
+
+const Title = ({title, subTitle}) => {
+  return (
+    <div className = 'section-title'>
+      <h2>
+        {title} <span>{subTitle}</span>
+      </h2>
+    </div>
+  )
+}
 export default Title;
  
