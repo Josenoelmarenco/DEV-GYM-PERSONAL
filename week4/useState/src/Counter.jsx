d294423ed885773a2421f800e877e1add0c86c5a
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-02-18 14:12:39 +0200

    creating counter in week4

---
+++ week4/useState/src/Counter.jsx
@@ -0,0 +1,18 @@
+//useState/src/Counter.jsx
+import { useState } from 'react';
+
+const Counter = () => {
+    // 1. Memoria
+    const [count, setCount] = useState(0);
+
+    // 2. Acción 
+    const handleClick = () => {
+        setCount(count + 1);
+    }; 
+    // 3. Vista
+    return (
+            <button onClick={handleClick}> Estos son mis clicks: {count} </button>
+        );
+};
+
+export default Counter;
