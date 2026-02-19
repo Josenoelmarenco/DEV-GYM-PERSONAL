892feb4e6e5c1e1c9d5a8ed983f954189bce1a7b
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-02-19 09:58:18 +0200

    counter

--- week4/w3-fe-lab1/src/Counter.jsx
+++ week4/w3-fe-lab1/src/Counter.jsx
@@ -1,17 +1,46 @@
-// import './Counter.css';
-
+import './Counter.css';
+import { useState } from 'react';
 
 const Counter = () => {
-    const handleClick = () => {
-        console.log('Button clicked');
-    };
-    
-    return (
-    <div className="content">
+    //1. Estados
+  const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState(0);
+
+    //2. Controladores
+  const setDarkThemeHandler = () => {
+    setTheme('dark');
+  };
+
+  const setLightThemeHandler = () => {
+    setTheme('light');
+  };
+
+  const toggleThemeHandler = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  const incrementHandler = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const decrementHandler = () => {
+    setCount(prvCount => prvCount - 1);
+  };
+
+  //3. Return en pantalla
+  return (
+    <div className={`content ${theme}`}>
       <h1>UseState Component</h1>
-      <button onClick={handleClick}>Dark</button>
-      <button onClick={handleClick}>Light</button>
+      <button onClick={setDarkThemeHandler}>Dark</button>
+      <button onClick={setLightThemeHandler}>Light</button>
+      <button onClick={toggleThemeHandler}>Toggle Theme {theme}</button>
+
+      <h2>Contador = {count}</h2>
+
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
     </div>
   );
 };
+
 export default Counter;
