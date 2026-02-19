14967dac18401c693802454a2953208fd545b04e
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-02-19 06:26:08 +0200

    const vs function

---
+++ week4/useState/src/ToDoList-v2.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const ToDoList = () => {
+    //1. Estado 
+    //2. controlar el input
+    //3. add tarea
+    //4. 
+    return (
+
+    );
+}
+
+export default useState;
