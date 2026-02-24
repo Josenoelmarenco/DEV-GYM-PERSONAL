9942e2d40f6f7d490acf4975b43518b3fa0b9c69
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-02-24 13:41:23 +0200

    refactor: convert Services to use local state
    - Replace imported data arrays with useState hooks
    - Maintain existing functionality with component state
    - Prepare components for dynamic data manipulation

--- week4/week4-fe-pp/src/components/Services.jsx
+++ week4/week4-fe-pp/src/components/Services.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react';
 import { services } from '../data'
 import Title from './Title'
 import Service from './Service'
 
 const Services = () => {
-  return (
-    <section className='section services' id='services'>
-      <Title title='our' subTitle='services' />
+  const [servicesData, setServicesData] = useState(services);
+    return (
+      <section className='section services' id='services'>
+        <Title title='our' subTitle='services' />
 
-      <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
-        })}
-      </div>
+        <div className='section-center services-center'>
+          {services.map((service) => {
+            return <Service {...service} key={service.id} />
+          })}
+        </div>
     </section>
   )
 }
